55324a79b4b3467b256b83d896c7ffe940364381
scrane <stetsen.crane.dev@gmail.com>
2026-09-03 16:52:21 -0400

    Added new Character class with some base variables.

---
+++ Character.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Character{
+    private String name;
+    private long maxHp;
+    private long currentHp;
+    private long attack;
+    private long defense;
+    ArrayList<String> actions = new ArrayList<String>();
+}
+
