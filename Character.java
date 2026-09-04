0ac1fabdbef07d8dff11a919ba02809feb1d9c05
scrane <stetsen.crane.dev@gmail.com>
2026-09-04 18:06:56 -0400

    Added getter and setter methods for character class

--- Character.java
+++ Character.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 
 class Character{
     private String name;
@@ -6,6 +6,43 @@ class Character{
     private long currentHp;
     private long attack;
     private long defense;
-    ArrayList<String> actions = new ArrayList<String>();
+    private String[] actions = {"Attack", "Defend", "Heal"};
+    
+    ///////////////////
+    // Setter methods/
+    /////////////////
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setMaxHp(long maxHp) {
+        this.maxHp = maxHp;
+    }
+    public void setCurrentHp(long currentHp) {
+        this.currentHp = currentHp;
+    }
+    public void setAttack(long attack) {
+        this.attack = attack;
+    }
+    public void setDefense(long defense) {
+        this.defense = defense;
+    }
+    //////////////////
+    //Getter methods/
+    ////////////////
+    public String getName(){
+        return name;
+    }
+    public long getMaxHp(){
+        return maxHp;
+    }
+    public long getCurrentHp(){
+        return currentHp;
+    }
+    public long getAttack(){
+        return attack;
+    }
+    public long getDefense(){
+        return defense;
+    }
 }
 
